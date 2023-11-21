40ae838c811928df506097902290109337a3f04f
yassine128 <yassineseddaoui@gmail.com>
2023-11-20 22:46:36 -0500

    server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const path = require('path'); // Add this line for path module
+const path = require('path'); 
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
@@ -9,7 +10,8 @@ const io = socketIo(server);
 
 let listOfUsers = []; 
 
-// Use path.join to construct the correct absolute path
+
+app.use(cors());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.get('/draw', (req, res) => {
@@ -17,7 +19,7 @@ app.get('/draw', (req, res) => {
 });
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
@@ -29,13 +31,6 @@ io.on('connection', (socket) => {
   socket.on('newDrawing', (start, end, color, width) => {
     io.emit('draw', start, end, color, width); 
   });
-
-  // socket.on('newUser', (username) => {
-  //   if (username !== "") {
-  //     listOfUsers.push(username); 
-  //     io.emit('user', listOfUsers); 
-  //   }
-  // }); 
 
   socket.on('messageSent', (message, username) => {
     io.emit('messageReceived', message, username); 
